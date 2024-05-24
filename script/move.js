58e3c1dcbf7816f8ce205632387f7adf5e3606d7
Benedek Sagi <0322sagibenedek@gmail.com>
2024-05-24 21:13:23 +0200

    commitSomething

---
+++ script/move.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", (event) => {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  var images = document.getElementsByClassName("gridImage");
+  var image = document.creat
+for(let image of images) {
+  image.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+    }
+
+
+  var span = document.getElementsByClassName("close")[0];
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+});
